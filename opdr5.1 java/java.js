aa25d1d7939c1662fb6b814bceba04353aedc82e
stijnvanstaalduinen <144112978+stijnvanstaalduinen@users.noreply.github.com>
2023-10-12 11:50:56 +0200

    Delete opdr5.1 java directory

--- opdr5.1 java/java.js
+++
@@ -1,8 +0,0 @@
-function veranderkleur(kleur)
-{   
- document.getElementById("kop1").style.color = kleur;
-}
-function verandertekst(tekst)
-{
-   document.getElementById("kop1").innerHTML = "welkom"
-}
